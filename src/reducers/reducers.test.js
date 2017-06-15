654583d9e8b7996474d404f12dc898f2062d941b
Nicolas Donna <donnanicolas@gmail.com>
2017-06-15 12:25:13 -0300

    add reducers test

---
+++ src/reducers/reducers.test.js
@@ -0,0 +1,82 @@
+import reducers from './index';
+
+import { authenticate, logout } from '../actions/auth';
+import { loadTracks, recommendTracks, trackLiked } from '../actions/track';
+import tracks from '../fixtures/tracks';
+
+const [trackOne, trackTwo] = tracks;
+const initialState = {
+  auth: {},
+  user: { loading: true },
+  track: {
+    tracks: [],
+    tracksById: {},
+    loading: true
+  }
+};
+
+describe('auth reducers', () => {
+  it('should set the token', () => {
+    const action = authenticate('token')
+    const state = reducers(initialState, action);
+    expect(state).toEqual({ ...initialState, auth: { authToken: 'token' } });
+  });
+
+  it('should set the token', () => {
+    const action = logout();
+    const state = reducers(initialState, action);
+    expect(state).toEqual(initialState);
+  });
+});
+
+describe('track reducer', () => {
+  it('should load tracks', () => {
+    const action = loadTracks(tracks);
+    const state = reducers(initialState, action);
+    expect(state).toEqual({
+      ...initialState,
+      track: {
+        tracks: [trackOne.id, trackTwo.id],
+        tracksById: {
+          [trackOne.id]: trackOne,
+          [trackTwo.id]: trackTwo,
+        },
+        loading: false,
+      }
+    });
+  });
+
+  it('should set loading true', () => {
+    const action = recommendTracks();
+    const initialStateWithTracks = {
+      ...initialState,
+      track: {
+        tracks: [trackOne.id, trackTwo.id],
+        tracksById: {
+          [trackOne.id]: trackOne,
+          [trackTwo.id]: trackTwo,
+        },
+        loading: false,
+      }
+    }
+    const state = reducers(initialStateWithTracks, action);
+    expect(state.track.loading).toEqual(true);
+  });
+
+  it('should set liked to true', () => {
+    const action = trackLiked(trackOne);
+    const initialStateWithTracks = {
+      ...initialState,
+      track: {
+        tracks: [trackOne.id, trackTwo.id],
+        tracksById: {
+          [trackOne.id]: trackOne,
+          [trackTwo.id]: trackTwo,
+        },
+        loading: false,
+      }
+    }
+    const state = reducers(initialStateWithTracks, action);
+    expect(state.track.tracksById[trackOne.id].liked).toEqual(true);
+  });
+});
